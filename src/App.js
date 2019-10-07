5df32aba4b9be53cbf86c3ff2dfa319012a5b1cd
PeterTong <tkwpeter21@gmail.com>
2019-10-08 00:36:35 +0800

    filtering state

--- src/App.js
+++ src/App.js
@@ -22,15 +22,25 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
   render() {
+		// make a new array, because don't want to modify the raw data (monsters array)
+		// Destructure concept. it pulls the properites off of the object and set them to constant
+		const { monsters, searchField } = this.state;
+		const filteredMonsters = monsters.filter( monster =>
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
+			);
+
     return (
       <div className="App">
 				{/* e  represent the synthetic event */}
 				{/* setState is a Asynchronous function, so the state will be updated  after entering text into search bar next times */}
+				{/* onChange is a synthetic event, in any time it will run this function setState once. 
+				not calling the render() method again and again since setState will trigger the render() method */}
+				{/* https://reactjs.org/docs/events.html */}
 				<input type='search' 
 							 placeholder='search monsters' 
 							 onChange={e => this.setState({ searchField: e.target.value })}>
 				 </input>
-        <CardList monsters={this.state.monsters}>
+        <CardList monsters={filteredMonsters}>
         
         </CardList>
       </div>
