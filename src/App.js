86781675dde05787f384e2c17dbc8e19c08dd511
PeterTong <tkwpeter21@gmail.com>
2019-10-07 16:16:05 +0800

    Card List Component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import logo from './logo.svg';
-import './App.css';
+import { CardList } from "./components/card-list/card-list.component";
+
+import "./App.css";
 
 // using class component, we can access this thing called state.
-// state is some object 
+// state is some object
 class App extends Component {
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		this.state = {
-			monsters: [
-				
-			]
-		}
-	}
+    this.state = {
+      monsters: []
+    };
+  }
 
-	componentDidMount(){
-		fetch('https://jsonplaceholder.typicode.com/users')
-		.then(response => response.json())
-		.then(users => this.setState({ monsters: users }));
-	}
-	render() {
-		return (
-			<div className="App">
-				{this.state.monsters.map( monster => (
-					<h1 key={monster.id}> { monster.name } </h1>
-				))}
-			</div>
-		);
-	}
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+  render() {
+    return (
+      <div className="App">
+        <CardList name="PeterTong">
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}> {monster.name} </h1>
+          ))}
+        </CardList>
+      </div>
+    );
+  }
 }
 
 // {'inside curly brackets is Javascript expression'}
