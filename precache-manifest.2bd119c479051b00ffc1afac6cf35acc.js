838dfe76f50d96be3e2d5660e22fad30070752b6
PeterTong <tkwpeter21@gmail.com>
2019-10-20 00:02:13 +0800

    Updates

---
+++ precache-manifest.2bd119c479051b00ffc1afac6cf35acc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9546758696916f0ecd9d648b1c5db5ff",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9ac41ca120f096c4dfd2",
+    "url": "/monsters-rolodex/static/css/main.742f1d82.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/monsters-rolodex/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "9ac41ca120f096c4dfd2",
+    "url": "/monsters-rolodex/static/js/main.581053f5.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
